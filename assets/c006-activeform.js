5d0b7c8e892aaa8b3dd4a35d2c6df915f9190394
Jon Chambers <jchambers.dev@gmail.com>
2014-12-13 16:49:01 -0800

    Updates

--- assets/c006-activeform.js
+++ assets/c006-activeform.js
@@ -6,23 +6,34 @@
 jQuery(function () {
 
     jQuery('.c006-activeform-toggle-container')
-        .unbind('click')
+        //.unbind('click')
         .bind('click',
               function () {
+                  var _transistion = 700;
                   var $this = jQuery(this);
                   var $on = $this.find('.c006-activeform-toggle-on');
                   var $off = $this.find('.c006-activeform-toggle-off');
                   var $input = $this.find('input[type=hidden]');
 
-                  if ($input.val() == undefined || $input.val() == "" || $input.val() * 1.00 < 1) {
+                  if (!$input.attr('item_init') || $input.val() == undefined || $input.val() == "") {
+                      if ($input.val()) {
+                          $input.val(0)
+                      } else {
+                          $input.val(1);
+                      }
+                      $on.switchClass('c006-activeform-on', 'c006-activeform-off', _transistion, "easeInOutQuad");
+                      $off.switchClass('c006-activeform-off', 'c006-activeform-on', _transistion, "easeInOutQuad");
+                      $input.attr('item_init', 1);
+                  } else if ($input.val() == "0") {
                       $input.val(1);
-                      $on.switchClass('c006-activeform-on', 'c006-activeform-off', 1000, "easeInOutQuad");
-                      $off.switchClass('c006-activeform-off', 'c006-activeform-on', 1000, "easeInOutQuad");
+                      $off.switchClass('c006-activeform-on', 'c006-activeform-off', _transistion, "easeInOutQuad");
+                      $on.switchClass('c006-activeform-off', 'c006-activeform-on', _transistion, "easeInOutQuad");
                   } else {
                       $input.val(0);
-                      $off.switchClass('c006-activeform-on', 'c006-activeform-off', 1000, "easeInOutQuad");
-                      $on.switchClass('c006-activeform-off', 'c006-activeform-on', 1000, "easeInOutQuad");
+                      $on.switchClass('c006-activeform-on', 'c006-activeform-off', _transistion, "easeInOutQuad");
+                      $off.switchClass('c006-activeform-off', 'c006-activeform-on', _transistion, "easeInOutQuad");
                   }
-              });
+              })
+        .trigger('click');
 
 });
