50d3f1b7e83ec78eccd3d55a50d759b3318b5a0d
Jon Chambers <jchambers.dev@gmail.com>
2015-08-03 22:16:56 -0700

    new options

--- assets/ActiveField.php
+++ assets/ActiveField.php
@@ -88,7 +88,6 @@ class ActiveField extends \yii\widgets\ActiveField
         $this->adjustLabelFor($options);
 
         return $this;
-
     }
 
 
@@ -111,6 +110,54 @@ class ActiveField extends \yii\widgets\ActiveField
         return ActiveFieldBase::hiddenInput($this->options);
 
 
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return $this
+     */
+    public function hiddenInput($options = [])
+    {
+        $renderParts = ArrayHelper::remove($options, 'renderParts', FALSE);
+        $options = array_merge($this->inputOptions, $options);
+        $this->adjustLabelFor($options);
+        $this->parts['{input}'] = Html::activeHiddenInput($this->model, $this->attribute, $options);
+        if ($renderParts) {
+            if (!in_array('label', $renderParts)) {
+                $this->parts['{label}'] = '';
+            }
+            if (!in_array('error', $renderParts)) {
+                $this->parts['{error}'] = '';
+            }
+            if (!in_array('hint', $renderParts)) {
+                $this->parts['{hint}'] = '';
+            }
+        } else {
+            $this->parts['{label}'] = $this->parts['{error}'] = $this->parts['{hint}'] = '';
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array $options
+     * @param bool|TRUE $enclosedByLabel
+     * @param string $on_label
+     * @param string $off_label
+     *
+     * @return mixed
+     */
+    public function onOff($options = [], $enclosedByLabel = TRUE, $on_label = 'ON', $off_label = 'OFF')
+    {
+        if (isset($options['label']) && !isset($this->parts['{label}'])) {
+            $this->parts['{label}'] = $options['label'];
+            if (!empty($options['labelOptions'])) {
+                $this->labelOptions = $options['labelOptions'];
+            }
+        }
+
+      return  ActiveField::dropDownList([$off_label, $on_label], $this->options);
     }
 
 }
